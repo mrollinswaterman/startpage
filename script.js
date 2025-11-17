6c9e515e4fd169a0c9f969a10832372f6783b7bd
mrollinswaterman <mrollinswaterman@uchicago.edu>
2025-11-17 11:28:54 +0100

    [QoL] page will now interrupt the typing animation to focus the searchbar if the user starts typing.

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ function getTime() {
     clock.html(`${currentTime}&nbsp${currentDate}`);
 }
 
+function checkDoneTyping(){
+    if ($("#typed").html() == $("#typed-strings").html().trim().slice(3,-4)){
+        clearInterval(check);
+        $("#typed").toggleClass('hidden-cursor', true);
+        setTimeout(() => {
+            showSearchbar();
+        }, 200);
+    }
+}
+
+var check = setInterval(checkDoneTyping, 10);
+
 function randomImage() {
     console.log("selecting random image");
     var i = Math.floor(Math.random() * 3);
@@ -22,10 +34,24 @@ function randomImage() {
     $("img").attr("src", `assets/${i}.jpg`);
 }
 
-document.body.onkeyup = function(e) {
-    if (e.key == " ") {
-        //$("input").get(0).focus();
+function searchbarHidden() {
+    return $("#searchbar").css("display") == 'none'
+}
+
+function showSearchbar(key=null) {
+    if (searchbarHidden()) {
+        $("#searchbar").css("display", "flex");
+        $("input").get(0).focus();
+        if (key && key != "Meta") {
+            $("#input").val(key);
+        }
     }
+
+}
+
+document.body.onkeyup = function(e) {
+    showSearchbar(e.key);
+    document.body.onkeyup = null;
 }
 
 function setup() {
